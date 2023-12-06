2396bad4af818782a9739148c5b63cb6cedfad2b
connerA-613 <74221595+connerA-613@users.noreply.github.com>
2023-12-05 20:03:27 -0500

    Fix: Aesthetic Changes
    - Changed purple and gold colors
    - Changed home button logo
    - Changed hover color on nav buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = (props) => {
         <div class="app">
                 <nav class="navbar navbar-expand-lg navbar-light fixed-top">
                     <a class="navbar-brand" href="">
-                        <img id="logo" src="https://www.wlu.ca/images/general/desktop_logo.png" alt="Laurier Logo" width="150" height="50" class="d-inline-block align-text-top"/>
+                        <img id="logo" src="/laurier-icon.svg" alt="Laurier Logo" width="150" height="50" class="d-inline-block align-text-top"/>
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
